34eac36662ac898a387cc38db4af99aadb5bb541
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-15 19:45:35 +0000

    Responsable Dashboard page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,9 +6,13 @@ export default function DashboardRootLayout({
     return (
         <div className="h-screen flex">
             {/*Left */}
-            <div className="w-[14%] bg-red-200">L</div>
+            <div className="w-[14%] md:w[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+                L
+            </div>
             {/*Right */}
-            <div className="w-[86%] bg-blue-200">R</div>
+            <div className="w-[86%] md:[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
+                R
+            </div>
         </div>
     );
 }
