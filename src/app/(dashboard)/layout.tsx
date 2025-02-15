1e950037553ec2c4da3def28b66b78df8eddba1d
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-15 21:13:44 +0000

    vertical structure

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "../../components/Menu";
+
 export default function DashboardRootLayout({
     children,
 }: Readonly<{
@@ -6,8 +10,15 @@ export default function DashboardRootLayout({
     return (
         <div className="h-screen flex">
             {/*Left */}
-            <div className="w-[14%] md:w[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-                L
+            <div className="w-[14%] md:w[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+                <Link
+                    href="/"
+                    className="flex items-center justify-center lg:justify-start gap-2"
+                >
+                    <Image src="/logo.png" alt="logo" width={32} height={32} />
+                    <span className="hidden lg:block"> IPM School</span>
+                </Link>
+                <Menu />
             </div>
             {/*Right */}
             <div className="w-[86%] md:[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
