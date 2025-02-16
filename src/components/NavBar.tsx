e1d9cae986a34cf69cb89caa63b49c079f8c538c
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-16 10:19:42 +0000

    Message Icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,12 +7,22 @@ const NavBar = () => {
                 <Image src="/search.png" alt="search" width={14} height={14} />
                 <input
                     type="text"
-                    placeholder="Search"
+                    placeholder="Search..."
                     className="border-none outline-none ml-2"
                 />
             </div>
             {/*Icons And User*/}
-            <div className=""> </div>
+            <div className="flex items-center gap-6">
+                {/*Message*/}
+                <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+                    <Image
+                        src="/message.png"
+                        alt="message"
+                        width={20}
+                        height={20}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
