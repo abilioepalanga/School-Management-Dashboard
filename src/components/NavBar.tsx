2b71c73bb8a7e602f6c097acf08c454b64d37d4e
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-16 00:44:16 +0000

    NavBar Structure

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
 const NavBar = () => {
-    return <div className="">NavBar</div>;
+    return (
+        <div className="flex items-center justify-between p-4 ">
+            {/* Search Bar*/}
+            <div className="hidden md:flex">
+                <Image src="/search.png" alt="search" width={14} height={14} />
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="border-none outline-none ml-2"
+                />
+            </div>
+            {/*Icons And User*/}
+            <div className=""> </div>
+        </div>
+    );
 };
 
 export default NavBar;
