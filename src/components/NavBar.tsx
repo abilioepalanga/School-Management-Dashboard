aeeab877c0858aba3461376814526199e2c3d840
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-16 14:33:39 +0000

    Create Unstructera card

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const NavBar = () => {
     return (
-        <div className="flex items-center justify-between p-4 ">
+        <div className="flex items-center  justify-between p-4 ">
             {/* Search Bar*/}
             <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
                 <Image src="/search.png" alt="search" width={14} height={14} />
