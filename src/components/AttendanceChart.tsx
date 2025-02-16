cf562f26022763785c739ed8ffad06eb670e69e1
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-16 23:57:44 +0000

    import BarChat component

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -1,34 +1,84 @@
 "use client ";
 
+import Image from "next/image";
+import {
+    ResponsiveContainer,
+    BarChart,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Legend,
+    Bar,
+    Rectangle,
+} from "recharts";
+
 const data = [
     {
         name: "Mon",
-        present: 4000,
-        absent: 2400,
+        present: 40,
+        absent: 24,
     },
     {
         name: "Tue",
-        present: 3000,
-        absent: 1398,
+        present: 30,
+        absent: 13,
     },
     {
         name: "wed",
-        present: 2000,
-        absent: 9800,
+        present: 20,
+        absent: 35,
     },
     {
         name: "Thu",
-        present: 2780,
-        absent: 3908,
+        present: 27,
+        absent: 39,
     },
     {
         name: "Fri",
-        present: 1890,
-        absent: 4800,
+        present: 18,
+        absent: 48,
     },
 ];
 
 const AttendanceChart = () => {
-    return <div className="">Attendance Chart</div>;
+    return (
+        <div className="">
+            <div className="">
+                <h1>Attendance</h1>
+                <Image src="/moreDark.png" alt="more" width={20} height={20} />
+            </div>
+            {/* Attendance Chart */}
+            <ResponsiveContainer width="100%" height="100%">
+                <BarChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar
+                        dataKey="pv"
+                        fill="#8884d8"
+                        activeBar={<Rectangle fill="pink" stroke="blue" />}
+                    />
+                    <Bar
+                        dataKey="uv"
+                        fill="#82ca9d"
+                        activeBar={<Rectangle fill="gold" stroke="purple" />}
+                    />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
+    );
 };
 export default AttendanceChart;
