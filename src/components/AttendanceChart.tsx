81b42e92d3b25fce9bb92c1487a36aefdfc52e9c
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-16 23:51:01 +0000

    Create week list

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -3,45 +3,28 @@
 const data = [
     {
         name: "Mon",
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
+        present: 4000,
+        absent: 2400,
     },
     {
         name: "Tue",
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
+        present: 3000,
+        absent: 1398,
     },
     {
         name: "wed",
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
+        present: 2000,
+        absent: 9800,
     },
     {
         name: "Thu",
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
+        present: 2780,
+        absent: 3908,
     },
     {
         name: "Fri",
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-    {
-        name: "Page F",
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
-    },
-    {
-        name: "Page G",
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
+        present: 1890,
+        absent: 4800,
     },
 ];
 
