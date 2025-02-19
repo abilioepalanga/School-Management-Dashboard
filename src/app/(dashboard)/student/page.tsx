a39e4a739c4a8cde4af2bcb9451626cc71d9fe89
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-19 08:36:11 +0000

    Add EventCalendar and Announcements Components

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,5 @@
-import Announcements from "@/components/Announcements";
-import BigCalendar from "@/components/BigCalender";
 import EventCalendar from "@/components/EventCalendar";
+import Announcements from "@/components/Announcements";
 
 const StudentPage = () => {
     return (
