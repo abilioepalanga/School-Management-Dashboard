36ad4e1d244f52e84ea35aa256f34166590fd54d
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-21 17:15:51 +0000

    Parent Page

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,5 +1,26 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalender";
+
 const ParentPage = () => {
-    return <div>ParentPage</div>;
+    return (
+        <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+            {/* LEFT */}
+            <div className="w-full xl:w-2/3">
+                <div className="h-full bg-white p-4 rounded-md">
+                    <h1 className="text-xl font-semibold">Schedule</h1>
+                    <p className="text-sm text-gray-500">
+                        This is the schedule for{""}
+                        <span className="text-blue-900">John Prizley</span>
+                    </p>
+                    <BigCalendar />
+                </div>
+            </div>
+            {/* RIGHT */}
+            <div className="w-full xl:w-1/3 flex flex-col gap-8">
+                <Announcements />
+            </div>
+        </div>
+    );
 };
 
 export default ParentPage;
