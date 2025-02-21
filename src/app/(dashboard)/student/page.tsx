e3a99515f55df0194ca0e8deedd134aa0fd0dbce
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-21 17:22:16 +0000

    This is the schedule for 4th grade student

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -9,7 +9,11 @@ const StudentPage = () => {
             {/* LEFT */}
             <div className="w-full xl:w-2/3">
                 <div className="h-full bg-white p-4 rounded-md">
-                    <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+                    <h1 className="text-xl font-semibold">Schedule </h1>
+                    <p className="text-sm text-gray-500">
+                        This is the schedule for 4th grade student. Class: {""}
+                        <span className="text-blue-900">B001</span>
+                    </p>
                     <BigCalendar />
                 </div>
             </div>
