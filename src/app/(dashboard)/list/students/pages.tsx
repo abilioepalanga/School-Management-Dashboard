0d0863b291634c6df416703172ba9d8d3e4a7246
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-24 23:28:06 +0000

    Add user info card section

--- src/app/(dashboard)/list/students/pages.tsx
+++ src/app/(dashboard)/list/students/pages.tsx
@@ -24,6 +24,70 @@ const SingleStudentPage = () => {
                 {/* SHORTCUTS */}
                 {/* (This part will be added later) */}
             </div>
+            <div className="w-full xl:w-2/3">
+                {/* TOP */}
+                <div className="flex flex-col lg:flex-row gap-4">
+                    {/* USER INFO CARD */}
+                    <div className="bg-lamaSky py-6 px-4 rounded-md flex-1 flex gap-4">
+                        <div className="w-1/3">
+                            <Image
+                                src="https://images.pexels.com/photos/5414817/pexels-photo-5414817.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                                alt=""
+                                width={144}
+                                height={144}
+                                className="w-36 h-36 rounded-full object-cover"
+                            />
+                        </div>
+                        <div className="w-2/3 flex flex-col justify-between gap-4">
+                            <h1 className="text-xl font-semibold">
+                                Cameron Moran
+                            </h1>
+                            <p className="text-sm text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur
+                                adipisicing elit.
+                            </p>
+                            <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                                    <Image
+                                        src="/blood.png"
+                                        alt=""
+                                        width={14}
+                                        height={14}
+                                    />
+                                    <span>A+</span>
+                                </div>
+                                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                                    <Image
+                                        src="/date.png"
+                                        alt=""
+                                        width={14}
+                                        height={14}
+                                    />
+                                    <span>January 2025</span>
+                                </div>
+                                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                                    <Image
+                                        src="/mail.png"
+                                        alt=""
+                                        width={14}
+                                        height={14}
+                                    />
+                                    <span>user@gmail.com</span>
+                                </div>
+                                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
+                                    <Image
+                                        src="/phone.png"
+                                        alt=""
+                                        width={14}
+                                        height={14}
+                                    />
+                                    <span>+1 234 567</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
