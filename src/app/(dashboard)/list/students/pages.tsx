536b212c30d5c13d72a74c91ac86ad4a0d0a383d
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-24 23:29:00 +0000

    Add shortcuts section in the right column

--- src/app/(dashboard)/list/students/pages.tsx
+++ src/app/(dashboard)/list/students/pages.tsx
@@ -140,30 +140,41 @@ const SingleStudentPage = () => {
         </div>
     </div>
     {/* CARD */}
-    <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-        <Image
-            src="/singleClass.png"
-            alt=""
-            width={24}
-            height={24}
-            className="w-6 h-6"
-        />
-        <div className="">
-            <h1 className="text-xl font-semibold">6A</h1>
-            <span className="text-sm text-gray-400">
-                Class
-            </span>
+    <div className="w-full xl:w-1/3 flex flex-col gap-4">
+    <div className="bg-white p-4 rounded-md">
+        <h1 className="text-xl font-semibold">Shortcuts</h1>
+        <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link
+                className="p-3 rounded-md bg-lamaSkyLight"
+                href="/"
+            >
+                Student&apos;s Lessons
+            </Link>
+            <Link
+                className="p-3 rounded-md bg-lamaPurpleLight"
+                href="/"
+            >
+                Student&apos;s Teachers
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href="/">
+                Student&apos;s Exams
+            </Link>
+            <Link
+                className="p-3 rounded-md bg-lamaSkyLight"
+                href="/"
+            >
+                Student&apos;s Assignments
+            </Link>
+            <Link
+                className="p-3 rounded-md bg-lamaYellowLight"
+                href="/"
+            >
+                Student&apos;s Results
+            </Link>
         </div>
     </div>
-    {/* BOTTOM */}
-<div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-    <h1>Student&apos;s Schedule</h1>
-    <BigCalendar />
 </div>
 
-</div>
-
-    );
 };
 
 export default SingleStudentPage;
