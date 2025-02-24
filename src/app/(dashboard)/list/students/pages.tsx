6444d04807f2da06c033321f31af1965c5c0f4ad
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2025-02-24 23:28:28 +0000

    Add student stats cards (Attendance, Grade, Lessons, Class)

--- src/app/(dashboard)/list/students/pages.tsx
+++ src/app/(dashboard)/list/students/pages.tsx
@@ -89,6 +89,74 @@ const SingleStudentPage = () => {
                 </div>
             </div>
         </div>
+        {/* SMALL CARDS */}
+<div className="flex-1 flex gap-4 justify-between flex-wrap">
+    {/* CARD */}
+    <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+        <Image
+            src="/singleAttendance.png"
+            alt=""
+            width={24}
+            height={24}
+            className="w-6 h-6"
+        />
+        <div className="">
+            <h1 className="text-xl font-semibold">90%</h1>
+            <span className="text-sm text-gray-400">
+                Attendance
+            </span>
+        </div>
+    </div>
+    {/* CARD */}
+    <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+        <Image
+            src="/singleBranch.png"
+            alt=""
+            width={24}
+            height={24}
+            className="w-6 h-6"
+        />
+        <div className="">
+            <h1 className="text-xl font-semibold">6th</h1>
+            <span className="text-sm text-gray-400">
+                Grade
+            </span>
+        </div>
+    </div>
+    {/* CARD */}
+    <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+        <Image
+            src="/singleLesson.png"
+            alt=""
+            width={24}
+            height={24}
+            className="w-6 h-6"
+        />
+        <div className="">
+            <h1 className="text-xl font-semibold">18</h1>
+            <span className="text-sm text-gray-400">
+                Lessons
+            </span>
+        </div>
+    </div>
+    {/* CARD */}
+    <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+        <Image
+            src="/singleClass.png"
+            alt=""
+            width={24}
+            height={24}
+            className="w-6 h-6"
+        />
+        <div className="">
+            <h1 className="text-xl font-semibold">6A</h1>
+            <span className="text-sm text-gray-400">
+                Class
+            </span>
+        </div>
+    </div>
+</div>
+
     );
 };
 
